f46a0b58429414d4585b1de951d30dc45c5cf728
peterradev <fstpepi@gmail.com>
2021-09-27 18:24:48 -0500

    Created a Testing Java class, where we can test our new code

---
+++ BookStore/src/com/ebook/view/BookStoreTests.java
@@ -0,0 +1,15 @@
+package com.ebook.view;
+
+import com.ebook.model.partner.Partner;
+
+public class BookStoreTests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Partner partner = new Partner();
+		partner.setFirstName("John");
+		System.out.println(partner.getFirstName());
+
+	}
+
+}
